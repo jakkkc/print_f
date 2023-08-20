c3970d8fea7332ee338328ee4b1c754e59500a0e
jakkkc <jacmwaniki@gmail.com>
2023-08-20 01:58:14 -0700

    debug main

--- main.h
+++ main.h
@@ -43,7 +43,7 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-/****************** FUNCTIONS ******************/
+/** FUNCTIONS **/
 
 /* Printing characters and strings */
 int print_char(va_list types, char buffer[],
@@ -114,5 +114,5 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif 
+#endif
 
